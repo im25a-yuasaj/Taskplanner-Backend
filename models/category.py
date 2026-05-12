651f772ec49363948635cdcbf833489d950a361d
im25a-birrerm <birrerm@bzz.ch>
2026-05-12 08:51:37 +0200

    models/category done

--- models/category.py
+++ models/category.py
@@ -1,6 +1,12 @@
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from database.database import Base
 
-class Category(BaseModel):
-    CategoryID: int
-    Category: str
-    IsActive: bool
+class Category(Base):
+    __tablename__ = "Category"
+
+    CategoryID = Column(Integer, primary_key=True)
+    Category = Column(String(255), unique=True)
+    IsActive = Boolean, default=True
+
+    def __repr__(self):
+        return f"<Category {self.Category}>"
