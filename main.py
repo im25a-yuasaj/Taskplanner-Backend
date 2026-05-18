b3c3cee5156f2e723d1f264db3ee92c79d61e1ce
Josh Yuasa <yuasaj@bzz.ch>
2026-05-18 11:47:31 +0200

    creating routes, schemas and model for file management

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import user, task, category, priority, progress, material
+from routes import user, task, category, priority, progress, material, file
 app = FastAPI()
 
 app.include_router(user.router)
@@ -8,6 +8,7 @@ app.include_router(category.router)
 app.include_router(priority.router)
 app.include_router(progress.router)
 app.include_router(material.router)
+app.include_router(file.router)
 
 @app.get("/")
 def read_root():
